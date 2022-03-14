1b8da9e91603f72e58c58e3ca90b4fc4b13787df
AbdelwahedAB <ababdelwahed112@gmail.com>
2022-03-14 08:59:15 -1100

    Add color picker; Assignment done

--- scripts/etch.js
+++ scripts/etch.js
@@ -1,27 +1,34 @@
 
 let grid = document.querySelector(".grid");
 let clear_btn = document.querySelector("#clear");
+let color_picker = document.querySelector("#color");
 
 let grid_elems = [];
+let color = "#000";
 
 clear_btn.addEventListener("click", ()=>{
     console.log("[INFO] clearing grid ...");
+    
+    let dimensions = NaN;
+    do{
+        dimensions = prompt("Please enter the width of the grid (max = 100) : ");
+        if(dimensions === null) return;
+        dimensions = parseInt(dimensions);
+    }while(dimensions < 0 || dimensions > 100 || isNaN(dimensions));
 
     //remove all grid elements
     grid_elems.forEach(element => {
         element.remove();
     });
-
     grid_elems = [];
 
-    let dimensions = NaN;
-    do{
-        dimensions = prompt("Please enter the width of the grid (max = 100) : ");
-        dimensions = parseInt(dimensions);
-    }while(dimensions < 0 || dimensions > 100 || isNaN(dimensions));
-    
     createGrid(dimensions);
 });
+
+color_picker.addEventListener("change", (e)=>{
+    color = e.target.value;
+});
+
 
 function createGrid(dim){
     for(let i = 0; i < dim; i++){
@@ -32,10 +39,7 @@ function createGrid(dim){
             div.className = "grid-item";
 
             div.addEventListener("mouseover", (e)=>{
-                e.target.classList.add("highlight");
-            });
-            div.addEventListener("mouseout", (e)=>{
-                e.target.classList.remove("highlight");
+                e.target.style.background = color;
             });
             row.appendChild(div);
             grid_elems.push(div);
